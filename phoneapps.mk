91dd10707aa2f1861b4e026ebc35467a5ca45565
pirmansah.otoluwa@gmail.com <pirmansah.otoluwa@gmail.com>
2015-05-06 00:37:02 -0700

    update patchrom setup

---
+++ phoneapps.mk
@@ -0,0 +1,49 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := ApplicationsProvider \
+		BasicDreams \
+		Bluetooth \
+		BluetoothExt \
+		Camera2 \
+		CellBroadcastReceiver \
+		CertInstaller \
+		DocumentsUI \
+		Galaxy4 \
+		HoloSpiralWallpaper \
+		HTMLViewer \
+		KeyChain \
+		LatinIME \
+		LiveWallpapers \
+		MagicSmokeWallpapers \
+		NfcNci \
+		PacProcessor \
+		PhaseBeam \
+		PhotoPhase \
+		PhotoTable \
+		PrintSpooler \
+		qcrilmsgtunnel \
+		QuickBoot \
+		shutdownlistener \
+		Stk \
+		TimeService \
+		UserDictionaryProvider \
+		VisualizationWallpapers \
+		WAPPushManager \
+
+private-phone-priv-apps := BackupRestoreConfirmation \
+		com.qualcomm.location \
+		DefaultContainerService \
+		DownloadProvider \
+		InputDevices \
+		ExternalStorageProvider \
+		MediaProvider \
+		FusedLocation \
+		PicoTts \
+		ProxyHandler \
+		SettingsProvider \
+		SharedStorageBackup \
+		Shell \
+		Tag \
+		ThemesProvider \
+		WallpaperCropper \
